212ec5902e97b9888b0332c1071b3d55fc72a826
Abhishek Sengar <abhisheksengar@Abhisheks-MacBook-Air.local>
2023-08-20 15:04:59 +0530

    updated tic-tac-toe

--- TIC-TAC-TOE/game.py
+++ TIC-TAC-TOE/game.py
@@ -9,7 +9,7 @@ class TicTacToe:
         self.current_winner = None
 
     def print_board(self):
-        # to just getting the rows
+        # just getting the rows
         for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
 
